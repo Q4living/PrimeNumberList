daed45ce27efbf7dbc2fc2ccb81a7dee2b578566
Q4living <thomasqian@yahoo.com>
2016-04-28 14:26:21 +0800

    Update the comment

--- prime_list.r
+++ prime_list.r
@@ -1,18 +1,18 @@
 prime_list_brutal <- function(n){
   start <- Sys.time()
-  p <- 0
-  i <- 2
+  p <- 0 #Begin from the beginning
+  i <- 2 #Begin at 2
   while (i < n){
     if(sum(i/1:i==i%/%1:i)==2) p <- c(p, i) #if i is only can be divided by itself and 1, then sum() will be 2
     i <- i + 1
   }
-  #print(unique(p))
+  print(unique(p))
   print(paste(format(Sys.time() - start, format = "%s"), " time spent"))
 }
 
 prime_list <- function(n){
   start <- Sys.time()
-  p <- c(2)
+  p <- c(2) #Begin the list with 2
   i <- 2
   while (i < n){
     t <- TRUE
@@ -25,6 +25,6 @@ prime_list <- function(n){
     if(t) p <- c(p, i)
     i <- i + 1
   }
-  print(c(1,p))
+  print(c(1,p)) #Add 1 back to the list
   print(paste(format(Sys.time() - start, format = "%s"), " time spent"))
 }
